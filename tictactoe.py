8bda4498f489c0fe110ce72e3483184eac3e1153
LenGa05 <148592825+LenGa05@users.noreply.github.com>
2025-04-21 20:39:17 -0400

    made the code to a gui format using the calcgui

--- tictactoe.py
+++ tictactoe.py
@@ -1,82 +1,102 @@
-print("Welcome to Tic Tac Toe!")
-def tic_tac_toe():
-    a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    p1_wins = 0
-    p2_wins = 0
-    
-    def table():
-        print("        |       |     ")
-        print("   " + str([1]) + "  |  " + str(a[2]) + "  |  " + str(a[3]))
-        print("________|_______|_______")
-        print("        |       |     ")
-        print("   " + str(a[4]) + "  |  " + str(a[5]) + "  |  " + str(a[6]))
-        print("________|_______|_______")
-        print("        |       |     ")
-        print("   " + str(a[7]) + "  |  " + str(a[8]) + "  |  " + str(a[9]))
-        print("        |       |     ")
+import tkinter as tk
+from tkinter import messagebox
 
-    def place_mark(player, mark):
-        while True:
-            player_turn = 1
-            position = input("player choose square (1-9): ")
-            if position.isdigit() and int(position) in range(1, 10) and str(a[int(position)]) == position:
-                a[int(position)] = mark
-                break
+class TicTacToe(tk.Frame):
+    """
+    A simple Tic Tac Toe app developed using the Tkinter GUI.
+    Matches layout and structure of calculator app.
+    """
+
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.turn = 1
+        self.mark = {1: "X", 0: "O"}
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.status = tk.Label(self, text="Player X's Turn", font=("Arial", 16), fg="white", bg="black")
+        self.status.grid(row=0, column=0, columnspan=3, pady=10)
+
+        self.one_btn = tk.Button(self, text="", width=9, height=4, font=("Arial", 24), fg="black", bg="white", command=lambda: self.play(1))
+        self.one_btn.grid(row=1, column=0, padx=5, pady=5)
+
+        self.two_btn = tk.Button(self, text="", width=9, height=4, font=("Arial", 24), fg="black", bg="white", command=lambda: self.play(2))
+        self.two_btn.grid(row=1, column=1, padx=5, pady=5)
+
+        self.three_btn = tk.Button(self, text="", width=9, height=4, font=("Arial", 24), fg="black", bg="white", command=lambda: self.play(3))
+        self.three_btn.grid(row=1, column=2, padx=5, pady=5)
+
+        self.four_btn = tk.Button(self, text="", width=9, height=4, font=("Arial", 24), fg="black", bg="white", command=lambda: self.play(4))
+        self.four_btn.grid(row=2, column=0, padx=5, pady=5)
+
+        self.five_btn = tk.Button(self, text="", width=9, height=4, font=("Arial", 24), fg="black", bg="white", command=lambda: self.play(5))
+        self.five_btn.grid(row=2, column=1, padx=5, pady=5)
+
+        self.six_btn = tk.Button(self, text="", width=9, height=4, font=("Arial", 24), fg="black", bg="white", command=lambda: self.play(6))
+        self.six_btn.grid(row=2, column=2, padx=5, pady=5)
+
+        self.seven_btn = tk.Button(self, text="", width=9, height=4, font=("Arial", 24), fg="black", bg="white", command=lambda: self.play(7))
+        self.seven_btn.grid(row=3, column=0, padx=5, pady=5)
+
+        self.eight_btn = tk.Button(self, text="", width=9, height=4, font=("Arial", 24), fg="black", bg="white", command=lambda: self.play(8))
+        self.eight_btn.grid(row=3, column=1, padx=5, pady=5)
+
+        self.nine_btn = tk.Button(self, text="", width=9, height=4, font=("Arial", 24), fg="black", bg="white", command=lambda: self.play(9))
+        self.nine_btn.grid(row=3, column=2, padx=5, pady=5)
+
+        self.buttons = {
+            1: self.one_btn, 2: self.two_btn, 3: self.three_btn,
+            4: self.four_btn, 5: self.five_btn, 6: self.six_btn,
+            7: self.seven_btn, 8: self.eight_btn, 9: self.nine_btn
+        }
+
+        self.reset_btn = tk.Button(self, text="Restart", width=28, height=2, font=("Arial", 14), command=self.reset)
+        self.reset_btn.grid(row=4, column=0, columnspan=3, pady=10)
+
+    def play(self, pos):
+        if str(self.board[pos]) == str(pos):
+            self.board[pos] = self.mark[self.turn % 2]
+            self.buttons[pos].config(text=self.board[pos], state="disabled")
+            if self.check_winner():
+                self.status.config(text=f"Player {self.board[pos]} Wins!")
+                messagebox.showinfo("Game Over", f"Player {self.board[pos]} Wins!")
+                self.disable_all()
+            elif all(isinstance(x, str) for x in self.board[1:]):
+                self.status.config(text="It's a draw!")
+                messagebox.showinfo("Game Over", "It's a draw!")
+                self.disable_all()
             else:
-                print("Invalid Entry, try again!")
-        table()
-    def check_winner():
-        combos = [
-            (1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(3,5,7)]
-        for x, y, z in combos:
-            if a[x] == a[y] == a[z]:
+                self.turn += 1
+                self.status.config(text=f"Player {self.mark[self.turn % 2]}'s Turn")
+
+    def check_winner(self):
+        wins = [
+            (1, 2, 3), (4, 5, 6), (7, 8, 9),
+            (1, 4, 7), (2, 5, 8), (3, 6, 9),
+            (1, 5, 9), (3, 5, 7)
+        ]
+        for x, y, z in wins:
+            if self.board[x] == self.board[y] == self.board[z]:
                 return True
         return False
 
-    print("Starting a new round...\n")
-    table()
-    """while True:
-        p1 = input("player 1 choose: X or 0: ")
-        p2 = input("player 2 choose: X or 0: ")
-        if p1 == p2:
-            print("INVALID ENTRY, both players chose the same option")
-        elif p1.lower() == "X" and p2 == "0" or p1.lower() == "0" and p2.lower() == "X":
-            break
-        else:
-            print("INVALID ENTRY, Choose 1 or 2")"""
+    def disable_all(self):
+        for btn in self.buttons.values():
+            btn.config(state="disabled")
 
-    player1_mark = "X" if p1 == "1" else "O"
-    player2_mark = "O" if player1_mark == "X" else "X"
-    player1_mark = "X"
-    player2_mark = "O"
-    turn = 1
+    def reset(self):
+        self.board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.turn = 1
+        for i in range(1, 10):
+            self.buttons[i].config(text="", state="normal")
+        self.status.config(text="Player X's Turn")
 
-    while True:
-        if turn % 2 != 0:
-            place_mark("Player 1", player1_mark)
-            if check_winner():
-                print("PLAYER 1 WINS!!")
-                break
-        else:
-            place_mark("Player 2", player2_mark)
-            if check_winner():
-                print("PLAYER 2 WINS!!")
-                break
-        if all(str(cell) not in "123456789" for cell in a[1:]):
-            print("It's a DRAW?")
-        turn += 1
-
-    restart = input("Do you want to play again, choose Yes or No: ")
-    if restart.lower() == "Yes":
-        tic_tac_toe()
-    elif restart == "no" or restart == "No":
-        print("Thanks for playing!")
-    else:
-        restart = input("INVALID ENTRY, choose Yes or No: ")
-        if restart == "yes" or restart == "Yes":
-            tic_tac_toe()
-        elif restart == "no" or restart == "No":
-            print("Thanks for playing!")
-
-
-tic_tac_toe()
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.configure(bg="black")
+    root.geometry("300x400")
+    root.title("Tic Tac Toe")
+    app = TicTacToe(root)
+    root.mainloop()
